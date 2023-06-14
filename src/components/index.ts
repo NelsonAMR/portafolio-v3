7ebdfd3a01f113d75258ec5f21be8bc504bb7e2b
NelsonAMR <nelson.macias99@gmail.com>
2023-06-14 17:37:23 -0600

    preparacion para seccion de proyectos y habilidades

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import Header from "./Header.astro";
 import Footer from "./Footer.astro";
 import Nav from "./Nav.astro";
 import Home from "./Home.astro";
+import Skills from "./Skills.astro";
+import Projects from "./Projects.astro";
 
-export { Header, Footer, Nav, Home };
+export { Header, Footer, Nav, Home, Skills, Projects };
