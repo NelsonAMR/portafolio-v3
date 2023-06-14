8e042c68fd65cfc4d7365fff68b8e3d005e5ba8a
NelsonAMR <nelson.macias99@gmail.com>
2023-06-14 17:26:07 -0600

    landing lista, con boton para descargar cv y modificaciones a estilos globales

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Header from "./Header.astro";
 import Footer from "./Footer.astro";
 import Nav from "./Nav.astro";
+import Home from "./Home.astro";
 
-export { Header, Footer, Nav };
+export { Header, Footer, Nav, Home };
