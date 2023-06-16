2baf4a3eb6c62ef337eac5ce8c5812386abb301b
NelsonAMR <nelson.macias99@gmail.com>
2023-06-15 22:54:06 -0600

    seccion de projectos primera version, para movil y escritorio

---
+++ src/utils/projects.ts
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    id: 1,
+    url: "https://my-ewallet.netlify.app/",
+    image: "/assets/projects/nucleus.PNG",
+    name: "Nucleus E-Wallet",
+    description:
+      "Landing page desarrollada con fines didacticos, haciendo uso de Javascript y SASS",
+    technologies: ["HTML", "SASS", "JavaScript"],
+  },
+  {
+    id: 3,
+    url: "https://my-ewallet.netlify.app/",
+    image: "/assets/projects/nucleus.PNG",
+    name: "Nucleus E-Wallet",
+    description:
+      "Landing page desarrollada con fines didacticos, haciendo uso de Javascript y SASS",
+    technologies: ["HTML", "SASS", "JavaScript"],
+  },
+  {
+    id: 3,
+    url: "https://my-ewallet.netlify.app/",
+    image: "/assets/projects/nucleus.PNG",
+    name: "Nucleus E-Wallet",
+    description:
+      "Landing page desarrollada con fines didacticos, haciendo uso de Javascript y SASS",
+    technologies: ["HTML", "SASS", "JavaScript"],
+  },
+];
