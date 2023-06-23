84fa1010a5761a9ad026bdd4b02624a4e1a478a4
NelsonAMR <nelson.macias99@gmail.com>
2023-06-22 18:43:35 -0600

    formulario funcional, con alertas remodeladas

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_EMAIL_KEY: string;
+  readonly PUBLIC_EMAIL_SERVICE: string;
+  readonly PUBLIC_EMAIL_TEMPLATE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
